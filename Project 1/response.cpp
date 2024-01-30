fa6737114d8111f8f69fb7273b83900441160f3c
awise03 <wise.ale@northeastern.edu>
2024-01-30 17:40:42 -0500

    response.cpp and response.hpp

--- Project 1/response.cpp
+++ Project 1/response.cpp
@@ -1,14 +1,32 @@
 #include <iostream>
 #include "response.hpp"
 
-response::response(){
-    n = 5;
-    m = 10;
+response::response(int c, int i){
+    correctVal = c;
+    incorrectPos = i;
 }
 
-response::response(int length, int range){
-    n = length;
-    m = range;
+int response::getCorrectVal(){
+    return correctVal;
 }
 
-bool response::operator==()
+int response::getIncorrectPos(){
+    return incorrectPos;
+}
+
+void response::setCorrectVal(int c){
+    correctVal = c;
+}
+
+void response::setIncorrectPos(int i){
+    incorrectPos = i;
+}
+
+bool response::operator==(const response& r1){
+    return correctVal == r1.correctVal && incorrectPos == r1.incorrectPos;
+}
+
+ostream & operator<<(ostream&out, const response& r1){
+    out << r1.correctVal << " " << r1.incorrectPos;
+    return out;
+}
