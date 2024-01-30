f8f1f3bd5fe421691561f4695d26e34e06d23a2f
allstarangel <132019991+allstarangel@users.noreply.github.com>
2024-01-30 17:08:27 -0500

    header

--- code.h
+++ code.h
@@ -9,7 +9,21 @@
 #ifndef CODE.H
 #define CODE.H
 
+#include <vector>
 
+class Code
+{
+    public:
+    Code(int n, int m);
+    void randomInitial();
+    int checkCorrect(const vector<int> &guess);
+    int checkIncorrect(const vector<int> &guess);
+
+
+
+    private:
+     
+}
 
 
 #endif
