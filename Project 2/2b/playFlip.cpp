d8320d34c7832f99c612b6c99f276899731d6d5b
awise03 <wise.ale@northeastern.edu>
2024-02-22 23:11:38 -0500

    added comments

--- Project 2/2b/playFlip.cpp
+++ Project 2/2b/playFlip.cpp
@@ -1,7 +1,7 @@
 //
-// Created by Jackymora Isa on 2/22/24.
+// Created by Jackymora Isa and Alexander Wise on 2/22/24.
 //
-
+#include <iostream>
 #include "playFlip.h"
 #include "deck.h"
 
@@ -10,7 +10,7 @@ using namespace std;
 void playFlip()
 {
     int score = 0;
-
+    // Print out game instructions
     cout << "Welcome to Flip game!" << endl;
     cout << "A deck of 52 cards will be shuffled, and the top 24 cards will be available for uou to draw from." << endl;
     cout << "Either select a card to turn over by inputing a number between 1 and 24." << endl;
@@ -23,24 +23,29 @@ void playFlip()
     cout << "Try to end the game with as many points as possible! if you flip all 24 cards,\n "
             "the deck will be reshuffled and a new set of 24 will be drawn, and the game will continue." << endl;
 
+    // Initialize a new deck and shuffle 3 times
     Deck d = Deck();
     d.shuffle();
     d.shuffle();
     d.shuffle();
 
-    //cout << d << endl;
+
     cout << endl;
     int cards = 24;
     string move = "";
+    // Loops until the player decides to end their move or the deck is empty.
     while(move != "stand" || cards != 0) {
         cout << "\nYour current score is " << score;
         cout << "\nYou have " << cards << " cards remaining.";
         cout << "\nWould you like to flip your card or stand (flip, stand)? " << endl;
     
         cin >> move;
+
         if(move == "flip") {
+            // Pulls a card from the top of the deck
             Card card = d.deal();
             cout << "\n Your card is: \n " << card << endl;
+            // Compares card value to determine the score to be added
             if(card.getValue() == 1) {
                 score += 10;
             } else if(card.getValue() >= 11 && card.getValue() <= 13){
@@ -53,10 +58,14 @@ void playFlip()
             if(card.getSuit() == "Heart") {
                 score ++;
             }
-        } else {
+            cards--;
+        // Breaks out if the player decides to stand
+        } else if (move == "stand"){
             break;
+        } else {
+            "Incorrect input, please try again";
         }
-        cards--;
+        
     }
 
     cout << "\nYour final score is: " << score << endl;
